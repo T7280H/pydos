822a90fe8c318fa85c5bffe8c72cd54a0a7f3f86
T7280H <amir.bzmara319@outlook.com>
2025-02-01 15:17:12 +0330

    Create banner.py

---
+++ install/system/banner/banner.py
@@ -0,0 +1,13 @@
+# banner.py
+def display_banner():
+    banner = """
+    ***************************************
+    *                                     *
+    *           Welcome to PyDOS!         *
+    *                                     *
+    ***************************************
+    """
+    print(banner)
+
+if __name__ == "__main__":
+    display_banner()
